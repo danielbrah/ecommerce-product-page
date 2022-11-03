3af875f677a6e226ed875d156516698160dae669
Daniel <daniel.jimenezfrancisco@gmail.com>
2022-11-03 16:02:53 -0500

    Added quantity feature.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const navMenu = document.getElementById('nav-menu')
 const navOpen = document.getElementById('menu-open')
 const navClose = document.getElementById('menu-close')
 const overlay = document.getElementById('overlay')
+const quantity = document.querySelector('[id=quantity] h2')
+const quantityAdd = document.querySelector('[id=quantity] [id=quantity-add]')
+const quantitySubtract = document.querySelector('[id=quantity] [id=quantity-subtract]')
 
 navOpen.addEventListener('click', (e) =>{
     navMenu.classList.toggle('active')
@@ -11,4 +14,15 @@ navOpen.addEventListener('click', (e) =>{
 navClose.addEventListener('click', (e) =>{
     navMenu.classList.toggle('active')
     overlay.classList.toggle('active')
+})
+
+quantityAdd.addEventListener('click', () =>{
+    quantity.textContent = Number(quantity.textContent) + 1
+})
+
+quantitySubtract.addEventListener('click', () =>{
+    if (Number(quantity.textContent == 0))
+        return
+    else
+        quantity.textContent = Number(quantity.textContent) - 1
 })
