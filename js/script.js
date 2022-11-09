f51800a33cd187b71efd3859885acd177990c45c
Daniel <daniel.jimenezfrancisco@gmail.com>
2022-11-09 15:51:31 -0600

    Changes.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const overlay = document.getElementById('overlay')
 const quantity = document.querySelector('[id=quantity] h2')
 const quantityAdd = document.querySelector('[id=quantity] [id=quantity-add]')
 const quantitySubtract = document.querySelector('[id=quantity] [id=quantity-subtract]')
+const imageSelection = Array.from(document.getElementById('product-image-selection').children)
 
 navOpen.addEventListener('click', (e) =>{
     navMenu.classList.toggle('active')
@@ -26,3 +27,30 @@ quantitySubtract.addEventListener('click', () =>{
     else
         quantity.textContent = Number(quantity.textContent) - 1
 })
+
+// Image selection
+imageSelection.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (element.classList.contains('active'))
+        {
+            return
+        }
+        else
+        {
+            element.classList.toggle('active')
+            imageSelection.forEach((element, imgIndex)=>{
+                if (imgIndex == index)
+                {
+                    return
+                }
+                else
+                {
+                    imageSelection[imgIndex].classList.remove('active')
+                    console.log(imgIndex)
+                }
+            })
+        }
+    })
+})
+
+
